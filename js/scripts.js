4ff2b762dce335225b52d49a95518065e68900ad
Ian Garner <icgarner21@gmail.com>
2017-05-12 11:09:02 -0700

    add business logic function intial

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// business logic
+function Pizza(pizzaId, size) {
+  this.pizzaId = pizzaId;
+  this.pizzaToppings = [];
+  this.pizzaSize = size;
+}
+
+var sizeCosts = {
+  "small": 800,
+  "medium": 1000,
+  "large": 1200
+};
+
+
+
+
+
+
+// UI logic

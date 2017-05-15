f8fab275748619e761261e27078fa1dc0b75d746
Ian Garner <icgarner21@gmail.com>
2017-05-15 01:40:39 -0700

    add better functionality in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,103 @@
-// business logic
-function Pizza(pizzaId, size) {
-  this.pizzaId = pizzaId;
-  this.pizzaToppings = [];
-  this.pizzaSize = size;
-}
+// Business logic
+var toppings = [];
 
-var sizeCosts = {
-  "small": 800,
-  "medium": 1000,
-  "large": 1200
+function Pizza(base, topping, cost) {
+  this.pizzaBases = [];
+  this.pizzaToppings = [];
+  this.pizzaCost = 0;
+}
+function Size(size) {
+  var sizes = [
+    {sizeName: "personal", basePrice: 6},
+    {sizeName: "medium", basePrice: 9},
+    {sizeName: "large", basePrice: 11},
+    {sizeName: "xlarge", basePrice: 14},
+  ]
+  function findSize(sz) {
+    for (s=0; s < sizes.length; s++) {
+      if (sizes[s].sizeName === size) {
+        return sizes[s];
+      }
+    }
+  }
+  var foundSize = findSize(size);
+  this.sizeName = foundSize.sizeName;
+  this.basePrice = foundSize.basePrice;
+}
+function Crust(crust) {
+  var crusts = [
+    {crustName: "original", basePrice: 1},
+    {crustName: "thincrust", basePrice: 2},
+    {crustName: "deepdish", basePrice: 3},
+  ];
+  function findCrust(cr) {
+    for (c=0; c < crusts.length; c++) {
+      if (crusts[c].crustName === crust) {
+        return crusts[c];
+      }
+    }
+  }
+  var foundCrust = findCrust(crust);
+  this.crustName = foundCrust.crustName;
+  this.basePrice = foundCrust.basePrice;
+}
+function Sauce(sauce) {
+  var sauces = [
+    {sauceName: "tomato", basePrice: 0},
+    {sauceName: "pesto", basePrice: 1},
+    {sauceName: "alfredo", basePrice: 2},
+    {sauceName: "garlic", basePrice: 0},
+  ];
+  function findSauce(sa) {
+    for (i=0; i < sauces.length; i++) {
+      if (sauces[i].sauceName === sauce) {
+        return sauces[i];
+      }
+    }
+  }
+  var foundSauce = findSauce(sauce);
+  this.sauceName = foundSauce.sauceName;
+  this.basePrice = foundSauce.basePrice;
+}
+function Topping(topping, price) {
+  this.toppingName = topping;
+  this.toppingPrice = price;
+}
+Pizza.prototype.baseAdd = function(base) {
+  this.pizzaBases.push(base);
 };
 
-
-
-
-
-
-// UI logic
-$(function() {
-  var pizzaIdCounter = 0;
-  var workingOrder = new Order();
-  var workingPizza = new Pizza(pizzaIdCounter, "Medium");
-  setupPizzaBuilder(workingPizza);
-  deliveryListener(workingOrder);
-
-  $("#" + pizza.pizzaSize).prop("checked", true);
-    $("input[value='cheese']").prop("checked", true);
-    pizza.pizzaToppings.push("cheese");
-    addToppingToPizzaBuilderList("cheese");
-    updatePizzaCost(pizza);
-    beginSizeListener(pizza);
-    beginToppingListener(pizza);
-
-$("#add-to-order").on("submit", function(event) {
-    event.preventDefault();
-    workingOrder.addPizza(workingPizza);
-    updateOrder(workingOrder);
-    pizzaIdCounter++;
-    workingPizza = new Pizza(pizzaIdCounter, "Medium");
-    setupPizzaBuilder(workingPizza);
+function toppingAssemble() {
+  var topArr = [];
+  ($('input[name=cheese]:checked')).each(function() {
+    var top = ($(this).val());
+    var thisTopping = new Topping(top, 2);
+    topArr.push(thisTopping);
   });
-
-var setupPizzaBuilder = function(pizza) {
-  $(".topping-checkboxes").empty();
-  $(".working-toppings").empty();
-  $(".topping-checkboxes").append(
-  "<div class='checkbox'>" +
-    "<label>" +
-      "<input type='checkbox' class='pizza-topping' value='" + topping + "'>" + topping + " " +
-    "</label>" +
-  "</div>"
-);
+  ($('input[name=veggie]:checked')).each(function() {
+    var top = ($(this).val());
+    var thisTopping = new Topping(top, 1);
+    topArr.push(thisTopping);
+  });
+  ($('input[name=meat]:checked')).each(function() {
+    var top = ($(this).val());
+    var thisTopping = new Topping(top, 3);
+    topArr.push(thisTopping);
+  });
+  return topArr;
 }
-});
+
+Pizza.prototype.pizzaCostCalc = function() {
+  var totalPrice = this.pizzaCost;
+  var baseArr = this.pizzaBases;
+  var topArr = this.pizzaToppings;
+  baseArr.forEach(function(base) {
+    var bPrice = base.basePrice;
+    totalPrice += bPrice;
+  });
+  topArr.forEach(function(top) {
+    var tPrice = top.toppingPrice;
+    totalPrice += tPrice;
+  });
+  this.pizzaCost = totalPrice;
+};
